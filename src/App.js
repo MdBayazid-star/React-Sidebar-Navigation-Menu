3efec6ef5c1a2941f8c44f26b72ae62f246e5c8b
MdBayazid-star <freelencerbayazid@gmail.com>
2022-06-03 11:02:29 +0600

    React Sidebar Navigation Menu

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Route, Switch, useLocation } from "react-router";
+import Sidebar from "./Sidebar";
+import Home from "./Pages/Home";
+import Team from "./Pages/Team";
+import Calender from "./Pages/Calender";
+import Documents from "./Pages/Documents";
+import Projects from "./Pages/Projects";
+import styled from "styled-components";
+import { AnimatePresence } from "framer-motion";
+
+const Pages = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: calc(2rem + 2vw);
+    background: linear-gradient(to right, #803bec 30%, #1b1b1b 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+
+function App() {
+  const location = useLocation();
+  return (
+    <>
+      <Sidebar />
+      <Pages>
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <Route exact path="/" component={Home} />
+            <Route path="/team" component={Team} />
+            <Route path="/calender" component={Calender} />
+            <Route path="/documents" component={Documents} />
+            <Route path="/projects" component={Projects} />
+          </Switch>
+        </AnimatePresence>
+      </Pages>
+    </>
+  );
+}
+
+export default App;
